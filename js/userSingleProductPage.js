cd4391e2a9d4a04473826d2e910afa7b8cb8ffd4
Priyanjith.N <143839723+Priyanjith-N@users.noreply.github.com>
2023-11-22 23:54:22 +0530

    Add files via upload

--- js/userSingleProductPage.js
+++ js/userSingleProductPage.js
@@ -16,19 +16,3 @@ const fliterMoblie = document.querySelector('.fliter-moblie');
 filterBtn.addEventListener('click', () => {
     fliterMoblie.classList.toggle('active');
 });
-
-// const smallWidth = document.querySelectorAll('.smallWidth');
-
-// function smallScreen() {
-//     if(window.outerWidth < 384){
-//         smallWidth.forEach(smClass => {
-//             smClass.classList.add('col-12');
-//         });
-//     }else{
-//         smallWidth.forEach(smClass => {
-//             smClass.classList.remove('col-12');
-//         });
-//     }
-// }
-
-// window.addEventListener("resize", smallScreen);
